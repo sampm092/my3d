5ebd5fe2f8aa8c4ab0d2f357ad1a44f275918bd8
SamPM092 <sammich4294@gmail.com>
2026-01-31 17:49:40 +0700

    refactor: enhance comments in PlayerMotor for clarity and maintainability

--- Assets/Scripts/PlayerMotor.cs
+++ Assets/Scripts/PlayerMotor.cs
@@ -15,7 +15,7 @@ public class PlayerMotor : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>(); // From Unity
     }
 
     /// <summary>
@@ -23,26 +23,34 @@ public class PlayerMotor : MonoBehaviour
     /// </summary>
     void Update()
     {
-        isGrounded = controller.isGrounded;
+        isGrounded = controller.isGrounded; // isGrounded = Was the CharacterController touching the ground during the last move?
     }
 
     // Menerima input dari InputManager.cs dan mengaplikasikannya pada Input Controller
     public void ProcessMove(Vector2 input)
     {
-        Vector3 moveDirection = Vector3.zero;
-        moveDirection.x = input.x;
-        moveDirection.z = input.y;
+        Vector3 moveDirection = Vector3.zero; // Initialize moveDirection value (0f,0f,0f)
+        moveDirection.x = input.x; // set moveDirection to (input.x,0,0)
+        moveDirection.z = input.y; // set moveDirection to (input.x,0,input.y)
+
+        //Move the character in the direction the player is facing [transform.TransformDirection(moveDirection)],
+        // at a given speed [... * speed],
+        // scaled by frame time [... * Time.deltaTime],
+        // while respecting collisions [controller.Move(...)].
         controller.Move(transform.TransformDirection(moveDirection) * speed * Time.deltaTime);
-        playerVelocity.y += gravity * Time.deltaTime;
+
+        playerVelocity.y += gravity * Time.deltaTime; // Apply gravity
+
         if (isGrounded && playerVelocity.y < 0)
             playerVelocity.y = -2f;
+
         controller.Move(playerVelocity * Time.deltaTime);
         Debug.Log(playerVelocity.y);
     }
 
     public void Jump()
     {
-        if (isGrounded)
+        if (isGrounded) //if touching ground
         {
             playerVelocity.y = Mathf.Sqrt(jumpHeight * gravity * -3.0f);
         }
