e1270f2d0b7af8d49b5045f1089b7e9345d1eca1
SamPM092 <sammich4294@gmail.com>
2026-02-12 16:07:27 +0700

    feat: add EventOnlyInteractable class and related materials

--- Assets/Editor/InteractableEditor.cs
+++ Assets/Editor/InteractableEditor.cs
@@ -6,16 +6,35 @@ public class InteractableEditor : Editor
     public override void OnInspectorGUI()
     {
         Interactable interactable = (Interactable)target;
-        base.OnInspectorGUI();
-        if (interactable.useEvents)
+        if (target.GetType() == typeof(EventOnlyInteractable))
         {
+            interactable.promptMessage = EditorGUILayout.TextField(
+                "Prompt Message",
+                interactable.promptMessage
+            );
+            EditorGUILayout.HelpBox(
+                "EventOnlyInteractable can only use Unity Events",
+                MessageType.Info
+            );
             if (interactable.GetComponent<InteractionEvent>() == null)
+            {
+                interactable.useEvents = true;
                 interactable.gameObject.AddComponent<InteractionEvent>();
+            }
         }
         else
         {
-            if (interactable.GetComponent<InteractionEvent>() != null)
-                DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+            base.OnInspectorGUI();
+            if (interactable.useEvents)
+            {
+                if (interactable.GetComponent<InteractionEvent>() == null)
+                    interactable.gameObject.AddComponent<InteractionEvent>();
+            }
+            else
+            {
+                if (interactable.GetComponent<InteractionEvent>() != null)
+                    DestroyImmediate(interactable.GetComponent<InteractionEvent>());
+            }
         }
     }
 }
