dde61e042db78222493b98764ad392eab60b4e98
SamPM092 <sammich4294@gmail.com>
2026-02-12 15:07:52 +0700

    feat: half 4ssed healthbar for now

---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private float health;
+    public int MaxHealth = 100;
+    public float ChipSpeed = 2f;
+    public Image BgHB;
+    public Image FrontHB;
+    public TextMeshProUGUI HealthText;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        health = MaxHealth;
+    }
+
+    void Update()
+    {
+        health = Mathf.Clamp(health, 0, MaxHealth);
+    }
+
+    public void UpdateHealthUI()
+    {
+        float fillF = FrontHB.fillAmount;
+        float fillB = BgHB.fillAmount;
+        float hFraction = health / MaxHealth;
+
+        if (fillB > hFraction)
+        {
+            FrontHB.fillAmount = hFraction;
+            BgHB.color = Color.red;
+        }
+    }
+}
