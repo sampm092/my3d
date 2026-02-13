960b1c80d1df1de33c2fc4046204ee5d0b477d10
SamPM092 <sammich4294@gmail.com>
2026-02-13 13:49:51 +0700

    feat: fiillHP bar implementation

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -11,7 +11,7 @@ public class PlayerHealth : MonoBehaviour
     public int MaxHealth = 100;
     public float ChipSpeed = 2f;
     public float lerpTimer;
-    public Image BgHB;
+    public Image backHB;
     public Image FrontHB;
     public TextMeshProUGUI HealthText;
 
@@ -31,28 +31,48 @@ public class PlayerHealth : MonoBehaviour
         {
             TakeDamage(Random.Range(5, 10));
         }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            fillHP(Random.Range(5, 10));
+        }
         UpdateHealthUI();
     }
 
     public void UpdateHealthUI()
     {
         float fillF = FrontHB.fillAmount;
-        float fillB = BgHB.fillAmount;
+        float fillB = backHB.fillAmount;
         float hFraction = health / MaxHealth;
 
         if (fillB > hFraction)
         {
             FrontHB.fillAmount = hFraction;
-            BgHB.color = Color.red;
+            backHB.color = Color.red;
             lerpTimer += Time.deltaTime;
             float percentComplete = lerpTimer / ChipSpeed;
-            BgHB.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+            percentComplete = percentComplete * percentComplete;
+            backHB.fillAmount = Mathf.Lerp(fillB, hFraction, percentComplete);
+        }
+        if (fillF < hFraction)
+        {
+            backHB.color = Color.green;
+            backHB.fillAmount = hFraction;
+            lerpTimer += Time.deltaTime;
+            float percentComplete = lerpTimer / ChipSpeed;
+            percentComplete = percentComplete * percentComplete;
+            FrontHB.fillAmount = Mathf.Lerp(fillF, backHB.fillAmount, percentComplete);
         }
     }
 
     public void TakeDamage(float damage)
     {
         health -= damage;
+        lerpTimer = 0f;
+    }
+
+    public void fillHP(float heal)
+    {
+        health += heal;
         lerpTimer = 0f;
     }
 }
