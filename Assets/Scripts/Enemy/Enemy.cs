dc2791c7bdf47932c4f8f4e695ea9c2577aa8f0a
SamPM092 <sammich4294@gmail.com>
2026-03-04 14:37:40 +0700

    feat: add sight distance and field of view to enemy, implement CanSeePlayer method

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -13,6 +13,9 @@ public class Enemy : MonoBehaviour
     [SerializeField]
     private string currentState;
     public Path path;
+    private GameObject player;
+    public float sightDistance = 20f;
+    public float fieldOfView = 85f;
 
     // Start is called before the first frame update
     void Start()
@@ -20,8 +23,26 @@ public class Enemy : MonoBehaviour
         statemachine = GetComponent<StateMachine>();
         agent = GetComponent<NavMeshAgent>();
         statemachine.Initialize();
+        player = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update() { }
+
+    public bool CanSeePlayer()
+    {
+        if (player != null)
+        {
+            if (Vector3.Distance(transform.position, player.transform.position) < sightDistance)
+            {
+                Vector3 targetDirection = player.transform.position - transform.position;
+                float angleToPlayer = Vector3.Angle(targetDirection, transform.forward);
+                if (angleToPlayer >= -fieldOfView && angleToPlayer <= fieldOfView)
+                {
+                    Ray ray = new Ray(transform.position, targetDirection);
+                }
+            }
+        }
+        return true; //wrong code
+    }
 }
